05ef4a732c1918941f87be3a9123a5b7b1785e4c
IvanLatysh@gmail.com <IvanLatysh@gmail.com@1803f72d-4e47-0410-80d3-43c50cacbe1c>
2008-10-10 17:25:07 +0000

    Added BlazeDS sample application.

---
+++ trunk/impl/samples/blazeds-webapp/web/WEB-INF/actions/src/SayHello.java
@@ -0,0 +1,20 @@
+import flex.messaging.messages.RemotingMessage;
+
+import javax.script.ScriptContext;
+
+/**
+ * Hello action
+ *
+ * @author Ivan Latysh
+ * @version 0.1
+ * @since 9-Oct-2008 8:04:36 PM
+ */
+public class SayHello {
+
+  public static Object eval(ScriptContext context) {
+    RemotingMessage message = (RemotingMessage) context.getAttribute("message", ScriptContext.ENGINE_SCOPE);
+    String name = (message.getParameters().size()>0 ?String.valueOf(message.getParameters().get(0)) :null);
+    return "Hello "+(null==name ?"there." :name);
+  }
+
+}
